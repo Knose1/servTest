28bd1f0e6bfd0da47f493d7e599a4b4133fadcb0
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-08-06 15:50:12 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine','ejs');
 
 app.get('/', function(request,response) {
-  response.render('pages/index');
+  response.render(`pages/${request.path}`);
 });
 
 app.get(/./, function(request,response) {
