8510c442566be290a76a1212fbaacc2684223047
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-08-06 15:57:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,13 +6,43 @@ app.set('port', (process.env.PORT || 0000));
 app.set('views', __dirname + '/views');
 app.set('view engine','ejs');
 
-app.get('/', function(request,response) {
+//404 Handeler
+app.all("*", function(request,response) {
+  response.status(404).send('Sorry, we cannot find that!');
+});
+
+
+app.get('/view/*', function(request,response) {
   response.render(`pages/${request.path}`);
 });
 
-app.get(/./, function(request,response) {
-  response.status(404).send('Sorry, we cannot find that!');
+app.get('/files/*', function(request,response,next) {
+  var options = {
+    root: __dirname /*+ '/public/'*/,
+    dotfiles: 'deny',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+  };
+
+  var fileName = request.path;
+  response.sendFile(fileName, options, function (err) {
+    if (err) {
+      next(err);
+    } else {
+      console.log('Sent:', fileName);
+    }
+  });
 });
+
+//Test
+app.get('/user/:id', function(request, response){
+  response.send('user ' + request.params.id);
+});
+// \Test
+
+
 
 app.listen(app.get('port'), function() {
   console.log(`App is running on port ${app.get('port')}`)
