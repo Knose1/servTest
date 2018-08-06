8732e0e970f587bc524957b60f121f18d90c760d
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-08-06 17:38:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,56 +6,15 @@ app.set('port', (process.env.PORT || 0000));
 app.set('views', __dirname + '/views');
 app.set('view engine','ejs');
 
-//Test
-app.get('/user/:id', function(request, response){
-  response.send('user ' + request.params.id);
+/*app.get('/user/connect', function(request, response){
+  response.render(`pages/login`);
+});*/
+
+app.use(function(req, res, next){
+  res.locals.user = req.user;
+  res.locals.authenticated = !req.user.anonymous;
+  next();
 });
-// \Test
-
-
-
-
-
-app.get('/view/*', function(request,response) {
-  response.render(`pages/${request.path.slice("/view/".length)}`);
-});
-
-app.get('/files/*', function(request,response,next) {
-  
-  const filePart = request.path.replace(/\//g,"/▲").replace(/:/g,":▼").split(/[/:]/g).map(m => {
-
-    if (m.indexOf("▲") == 0)
-      return {content: m.replace(/▲/g,""), type:"/"};
-    else if (m.indexOf("▼") == 0)
-      return {content: m.replace(/▼/g,""), type:":"};
-
-  }).filter(f => f != undefined) //return an array
-  filePart.shift();
-  
-  console.log(filePart)
-  
-  let filtered = filePart.filter(f => f.type == "/")
-  let popped = filtered.pop()
-  
-  var options = {
-    root: __dirname + filtered.map(m => m.type + m.content).join(""),
-    dotfiles: 'deny',
-    headers: {
-        'x-timestamp': Date.now(),
-        'x-sent': true
-    }
-  };
-
-  var fileName = popped.content;
-  response.sendFile(fileName, options, function (err) {
-    if (err) {
-      next(err);
-    } else {
-      console.log('Sent:', fileName);
-    }
-  });
-});
-
 
 //404 Handeler
 app.all("*", function(request,response) {
