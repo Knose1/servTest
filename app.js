4220ac8fad6268220bf1ac73527c8b54fc50e9e9
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-08-06 20:19:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var multer = require('multer'); // v1.0.5
+var multer = require('multer');
 var upload = multer(); // for parsing multipart/form-data
 
 app.use(bodyParser.json()); // for parsing application/json
