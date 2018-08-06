747b396096f130b02e113fe9c93123d2348515a0
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-08-06 19:35:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ app.get(`/admin/eval`,function(request,response) {
 
 app.get(`/admin/*`,function(request,response,next) {
   if (request.query.admin == encodeURI(process.env.ADMINPASS) && request.query.mail == encodeURI(process.env.ADMINMAIL) ) {
-      app.locals.adminIp = request.ip
+    app.locals.adminIp = request.ip;
+    console.log("▬▬",app.locals.adminIp,"▬▬");
+    response.send("Successfuly connected")
   } else {
       response.sendStatus(403);
   }
@@ -24,8 +26,8 @@ app.get(`/admin/*`,function(request,response,next) {
 });
 //404 Handeler
 app.all("*", function(request,response) {
-  response.status(403).send({error: "Accès refusé"})
   response.status(404).send({error: "404 not found", path: request.path});
+  response.status(403).send({error: "Accès refusé"});
 });
 
 app.listen(app.get('port'), function() {
