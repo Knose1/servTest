a86b2d7526a881abe35cd80144351332484c175a
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-08-10 12:08:53 +0200

    Create file.js

---
+++ script/app/file.js
@@ -0,0 +1,37 @@
+exports.execute = () => {
+    //Handle CSS
+    app.get(`/css/*.css`,function(request,response,next) {
+        var options = {
+            root: __root,
+        };
+        var fileName = "/views" + request.path
+        response.sendFile(fileName, options, function (err) {
+            if (err) {
+                console.log(fileName)
+                console.error(err)
+                response.status(404)
+            } else {
+                console.log('Sent:', fileName);
+                response.status(200)
+            }
+        });
+    });
+    
+    //Handle MP4
+    app.get(`/mp4/*.mp4`,function(request,response,next) {
+        var options = {
+            root: __root,
+        };
+        var fileName = "/files" + request.path
+        response.sendFile(fileName, options, function (err) {
+            if (err) {
+                console.log(fileName)
+                console.error(err)
+                response.status(404)
+            } else {
+                console.log('Sent:', fileName);
+                response.status(200)
+            }
+        });
+    });
+}
