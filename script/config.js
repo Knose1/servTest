ece2f836c1638dbb3fd97163dbe59b8dbe1a02ba
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-08-10 11:47:05 +0200

    Update config.js

--- script/config.js
+++ script/config.js
@@ -1,3 +1,8 @@
 exports.load = () => {
-    
+    return  express = require('express'),
+            app = express(),
+            bodyParser = require('body-parser'),
+            multer = require('multer'),
+            cookieParser = require('cookie-parser'),
+            upload = multer(); // for parsing multipart/form-data
 }
