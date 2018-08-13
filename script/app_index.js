cffebba9d41ac82d2527b14af5c8173e76bbe729
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-08-14 00:35:10 +0200

    Update app_index.js

--- script/app_index.js
+++ script/app_index.js
@@ -102,9 +102,9 @@ app.listen(app.get('port'), function() {
         app.locals.adminRandCode${i} = randNum;
         strCodes.push(randNum);
         console.log("▬▬",randNum,"▬▬");
-        UriCodes += \`a${i}=\${randNum}\`;
+        UriCodes += \`a${i}=\${randNum}$\n\`;
       `)
     }
-    console.log("▬▬",UriCodes.slice(0,-1),"▬▬")
+    console.log("▬▬",UriCodes.slice(0,-2),"▬▬")
   app.locals.adminConnected = false
 });
