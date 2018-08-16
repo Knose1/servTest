46bf671b0a87bbf43f4a2d906c2fce1ce3ac0f3e
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-08-16 18:34:30 +0200

    Update testsocket.js

--- script/test/testsocket.js
+++ script/test/testsocket.js
@@ -1,22 +1,3 @@
 exports.execute = () => {
-  app.use(function (req, res, next) {
-    console.log('middleware');
-    req.testing = 'testing';
-    return next();
-  });
-
-  app.get('/', function(req, res, next){
-    console.log(expressWs.getWss().clients)
-    console.log('get route', req.testing);
-    res.end();
-  });
-
-  app.ws('/', function(ws, req) {
-    console.log(ws.getWss().clients)
-    ws.on('message', function(msg) {
-      console.log(msg);
-    });
-    console.log('socket', req.testing);
-  });
-
-} 
+  
+}
